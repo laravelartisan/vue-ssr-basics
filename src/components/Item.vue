27df56dcf891f5d7a82feda42f1233b08aa9639e
Nazim <Nazim>
2018-05-17 18:57:15 +0600

    store added

---
+++ src/components/Item.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>{{ item.title }}</div>
+</template>
+
+<script>
+    export default {
+        asyncData ({ store, route }) {
+        // return the Promise from the action
+        return store.dispatch('fetchItem', route.params.id)
+    },
+
+    computed: {
+        // display the item from store state.
+        item () {
+            return this.$store.state.items[this.$route.params.id]
+        }
+    }
+    }
+</script>
